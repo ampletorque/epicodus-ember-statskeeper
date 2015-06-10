b7857b32b9deae124ac30c72adf0a04047e402f5
Kim Dang & andrew david burt <student@epicodus.com>
2015-06-10 15:39:53 -0700

    add teams, players, hits, misses, and display all of them.

--- controllers/player.js
+++ controllers/player.js
@@ -1,10 +1,22 @@
+StatsKeeper.Player = Ember.Object.extend({
+  countBaskets: function() {
+    return 5;
+  }.property()
+});
+
+
 StatsKeeper.PlayerController = Ember.ObjectController.extend({
   needs: ['player'],
+
   actions: {
     success: function() {
       var player = this.get('controllers.player.model');
+      var successes = player.get('countBaskets');
+  alert(player.name);
       player.get('shots').pushObject(true);
       player.save();
+  player.successes.pushObject('player.successes', 5);
+  alert(player);
     },
     miss: function() {
       var player = this.get('controllers.player.model');
--- models/player.js
+++ models/player.js
@@ -2,5 +2,7 @@ StatsKeeper.Player = DS.Model.extend({
   name: DS.attr(),
   shots: DS.attr(), //array of booleans
   points: DS.attr(),
+  successes: DS.attr(),
+  fails: DS.attr(),
   Team: DS.belongsTo('team', {async: true})
 });
