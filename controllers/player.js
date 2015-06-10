908a2fd105caec1e6c118bbb80e77b436b6ee191
Kim Dang & andrew david burt <student@epicodus.com>
2015-06-10 15:54:39 -0700

    adds points counter

--- controllers/player.js
+++ controllers/player.js
@@ -6,6 +6,12 @@ StatsKeeper.Player = Ember.Object.extend({
   }.property()
 });
 
+StatsKeeper.Player = Ember.Object.extend({
+  points: function() {
+    return 5;
+  }.property()
+});
+
 
 StatsKeeper.PlayerController = Ember.ObjectController.extend({
   needs: ['player'],
@@ -13,18 +19,17 @@ StatsKeeper.PlayerController = Ember.ObjectController.extend({
   actions: {
     success: function() {
       var player = this.get('controllers.player.model');
-      var successes = player.get('countBaskets');
-
-      var score = player.get('successes');
-      player.set('successes', score+1);
-
+      // var successes = player.get('countBaskets');
+      var successes = player.get('successes');
+      player.set('successes', successes+1);
+      player.set('points', ((successes+1)*2));
       player.get('shots').pushObject(true);
       player.save();
     },
     miss: function() {
       var player = this.get('controllers.player.model');
-      var score = player.get('fails');
-      player.set('fails', score+1);
+      var fails = player.get('fails');
+      player.set('fails', fails+1);
       player.get('shots').pushObject(false);
       player.save();
     }
